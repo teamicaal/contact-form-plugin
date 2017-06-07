1637b0fe5191dc5e7b7d53867555a8fa7cc7eb3e
ashdotguru <hello@ash.guru>
2017-06-07 09:38:43 +0100

    allow custom to address

--- admin/class-icaal-contact-form-admin.php
+++ admin/class-icaal-contact-form-admin.php
@@ -134,7 +134,7 @@ class Icaal_Contact_Form_Admin {
 				$errors['email'] = 'Please enter a valid email';
 			}
 
-			if( !empty($required_fields) ) {
+			if( !empty($required) && !empty($required_fields) ) {
 				foreach( $required_fields as $field ) {
 					if( empty($data[$field]) ) {
 						$errors[$field] = ucwords( str_replace('_', ' ', $field) ) . ' is empty';
@@ -207,6 +207,10 @@ class Icaal_Contact_Form_Admin {
 	private function send_submission( $data ) {
 
 		$to = ICAAL_TO_ADDRESS;
+		if( !empty($data['to']) ) {
+			$to = $data['to'];
+			unset($data['to']);
+		}
 		$subject = 'New Website Enquiry';
 
 		$message = '<p>You have received a new enquiry from your website.</p>';
@@ -254,7 +258,7 @@ class Icaal_Contact_Form_Admin {
 	}
 
 	private function get_ip_address() {
-	  if (!empty($_SERVER['HTTP_CLIENT_IP']) && validate_ip($_SERVER['HTTP_CLIENT_IP'])) {
+	  if (!empty($_SERVER['HTTP_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_CLIENT_IP'])) {
 	    return $_SERVER['HTTP_CLIENT_IP'];
 	  }
 
@@ -262,21 +266,21 @@ class Icaal_Contact_Form_Admin {
 	    if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',') !== false) {
 	      $iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
 	      foreach ($iplist as $ip) {
-	        if (validate_ip($ip))
+	        if ($this->validate_ip($ip))
 	          return $ip;
 	      }
 	    } else {
-	      if (validate_ip($_SERVER['HTTP_X_FORWARDED_FOR']))
+	      if ($this->validate_ip($_SERVER['HTTP_X_FORWARDED_FOR']))
 	        return $_SERVER['HTTP_X_FORWARDED_FOR'];
 	    }
 	  }
-	  if (!empty($_SERVER['HTTP_X_FORWARDED']) && validate_ip($_SERVER['HTTP_X_FORWARDED']))
+	  if (!empty($_SERVER['HTTP_X_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_X_FORWARDED']))
 	    return $_SERVER['HTTP_X_FORWARDED'];
-	  if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+	  if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
 	    return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-	  if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
+	  if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && $this->validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
 	    return $_SERVER['HTTP_FORWARDED_FOR'];
-	  if (!empty($_SERVER['HTTP_FORWARDED']) && validate_ip($_SERVER['HTTP_FORWARDED']))
+	  if (!empty($_SERVER['HTTP_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_FORWARDED']))
 	    return $_SERVER['HTTP_FORWARDED'];
 
 	  return $_SERVER['REMOTE_ADDR'];
